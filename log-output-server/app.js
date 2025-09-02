2a0c4c07756b82301a19edb28f43338bd1e8eb46
Aleksi <aleksi.suoranta@gmail.com>
2025-09-02 22:47:33 +0300

    E2.1: Request pongcount using fetch instead of reading from volume

--- log-output-server/app.js
+++ log-output-server/app.js
@@ -3,14 +3,21 @@ const fs = require("node:fs");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     try {
+        console.log("Handling request...");
         const timestamp = fs.readFileSync('/usr/src/app/files/timestamp.txt', 'utf-8')
-        const pongCount = fs.readFileSync('/usr/src/app/files/pong.txt', 'utf-8')
+        const ppRes = await fetch("http://pong-svc:3000/pingpong", {
+            method: "GET",
+        });
+        const body = await ppRes.text();
+        const pongCount = Number(body);
+        //const pongCount = fs.readFileSync('/usr/src/app/files/pong.txt', 'utf-8')
         const response = `<div>${timestamp}</div> <div>${pongCount}</div>`
         res.set('Content-Type', 'text/html');
         res.send(Buffer.from(response))
-    } catch {
+    } catch (e) {
+        console.error(e);
         res.sendStatus(404)
     }
 })
