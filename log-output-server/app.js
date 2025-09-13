917f5fe997c075ced8346ccca4363105f67f40ba
Aleksuo <aleksi.suoranta@gmail.com>
2025-09-14 01:48:41 +0300

    E2.5

--- log-output-server/app.js
+++ log-output-server/app.js
@@ -6,6 +6,7 @@ const port = 3000;
 app.get('/', async (req, res) => {
     try {
         console.log("Handling request...");
+        const information = fs.readFileSync('/usr/src/app/config/information.txt', 'utf-8')
         const timestamp = fs.readFileSync('/usr/src/app/files/timestamp.txt', 'utf-8')
         const ppRes = await fetch("http://pong-svc:3000/pingpong", {
             method: "GET",
@@ -13,7 +14,12 @@ app.get('/', async (req, res) => {
         const body = await ppRes.text();
         const pongCount = Number(body);
         //const pongCount = fs.readFileSync('/usr/src/app/files/pong.txt', 'utf-8')
-        const response = `<div>${timestamp}</div> <div>${pongCount}</div>`
+        const response = `
+        <div>file content: ${information}</div>
+        <div>env variable: MESSAGE=${process.env.MESSAGE}</div>
+        <div>${timestamp}</div> 
+        <div>${pongCount}</div>
+        `
         res.set('Content-Type', 'text/html');
         res.send(Buffer.from(response))
     } catch (e) {
