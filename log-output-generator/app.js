befd75fe6c56ade6cff2d7e6eb4318ba9c678681
Aleksuo <aleksi.suoranta@gmail.com>
2025-09-13 22:59:52 +0300

    Add configmap for generator

--- log-output-generator/app.js
+++ log-output-generator/app.js
@@ -1,10 +1,10 @@
 const fs = require('node:fs')
-
+const timestampPath = process.env.TIME_STAMP_PATH
 const randomString = Math.random().toString(36).substring(2, 15);
 setInterval(() => {
     const timeStamp = new Date().toISOString();
     const res = `${timeStamp}: ${randomString}`
-    fs.writeFile('/usr/src/app/files/timestamp.txt', res, err => {
+    fs.writeFile(timestampPath, res, err => {
         if(err) {
             console.error(err)
         } else {
